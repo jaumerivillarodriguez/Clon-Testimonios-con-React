0a47adccb2409b979aea48d98f56cb6150c75226
jaumerivillarodriguez <jaumerivilla@gmail.com>
2022-12-07 18:02:18 +0100

    Primero proyecto en React

--- src/App.js
+++ src/App.js
@@ -7,8 +7,35 @@ function App() { //componente principal
     <div className="App">
       <div className='contenedor-principal'>
         <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-        <Testimonio />
+        <Testimonio
+          nombre = 'Shawn Wang'
+          pais = 'Singapur'
+          imagen = 'shawn'
+          testimonio = 'Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando en los cientos de horas de lecciones gratuitas en freeCodeCamp. En un año tenía un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.'
+          cargo = 'Ingeniero de software'
+          empresa  = 'Amazon' />
+
+        <Testimonio
+          nombre = 'Sara Chima'
+          pais = 'Nigeria'
+          imagen = 'sarah'
+          testimonio = 'freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mi conocimiento de codificación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.'
+          empresa  = 'ChatDesk' />
+
+        <Testimonio
+          nombre = 'Emma Bostian'
+          pais = 'Suecia'
+          imagen = 'emma'
+          testimonio = 'Siempre he tenido problemas para aprender JavaScript. Tomé muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como un ingeniero de software en Spotify.'
+          cargo = 'Ingeniero de software'
+          empresa  = 'Spotify' />
+
+          
+
+        
       </div>
+
+
       
     </div>
   );
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -2,18 +2,18 @@ import React from 'react';
 import '../css/Testimonio.css';
 
 
-function Testimonio () {
+function Testimonio (props) {
   return(
     <div className= 'contenedor-testimonio'>
       <img 
       className='imagen-testimonio'
-      src = {require('../imagenes/testimonio-emma.png')}
+      src = {require(`../imagenes/testimonio-${props.imagen}.png`)}
       alt = 'Foto de Emma'/>
 
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>Emma Bostian en Suecia</p>
-        <p className='cargo-testimonio'>Ingeniera de Software en Spotify</p>
-        <p className='texto-testimonio'>"Siempre he tenido problemas para aprender JavaScript. He tomado mucho cursos, pero el curso de FreeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniera de software en Spotify"</p>
+        <p className='nombre-testimonio'><strong>{props.nombre}</strong> en {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong> </p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
       </div>
 
     </div>
