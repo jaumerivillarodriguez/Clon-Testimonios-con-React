6c10117fe5e86c3677742b1e049cf6fd32267130
jaumerivillarodriguez <jaumerivilla@gmail.com>
2022-12-07 18:21:56 +0100

    Reto de Proyecto

---
+++ src/personas.js
@@ -0,0 +1,26 @@
+export const Shawn = {
+    nombre : 'Shawn Wang',
+    pais : 'Singapur',
+    imagen : 'shawn',
+    testimonio : 'Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando en los cientos de horas de lecciones gratuitas en freeCodeCamp. En un año tenía un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.',
+    cargo : 'Ingeniero de software',
+    empresa  : 'Amazon'
+}
+
+export const Sara = {
+    nombre : 'Sara Chima',
+    pais : 'Nigeria',
+    imagen :'sarah',
+    testimonio : 'freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mi conocimiento de codificación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.',
+    cargo : 'Ingeniera de software',
+    empresa  : 'ChatDesk'
+}
+
+export const Emma = {
+    nombre : 'Emma Bostian',
+    pais : 'Suecia',
+    imagen : 'emma',
+    testimonio : 'Siempre he tenido problemas para aprender JavaScript. Tomé muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como un ingeniero de software en Spotify.',
+    cargo : 'Ingeniero de software',
+    empresa  : 'Spotify'
+}
